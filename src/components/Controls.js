ce80feb559bd5ef4384af00fc6454817e3595827
HortTemppa <temelameni@gmail.com>
2020-01-06 12:28:23 +0200

    testing that the new branch is working

---
+++ src/components/Controls.js
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import { extend, useThree, useRender, Canvas } from "react-three-fiber";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+extend({ OrbitControls });
+
+const Controls = ({ handleKeydown, keydown }) => {
+  //const camera = useRef();
+  const controls = useRef();
+
+  const { camera } = useThree();
+  const { gl } = useThree();
+
+  useRender(() => controls.current.update());
+
+  return (
+    <group>
+      <orbitControls
+        ref={controls}
+        controls={{
+          keys: true
+        }}
+        args={[camera, gl.domElement]}
+        enableDamping
+      />
+    </group>
+  );
+};
+
+export default Controls;
